d4b38135d19cd85b651e4b896d63e38e32781fa3
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-10 22:48:24 -0300

    removed .env from git

--- src/mysql.ts
+++ src/mysql.ts
@@ -4,10 +4,10 @@ config();
 
 const pool = mysql.createPool({
     "user": process.env.USER_DATABASE,
-    "password":'mendes',
-    "database":'api-youtube-project',
-    "host":'localhost',
-    "port": 3306
+    "password": process.env.PASSWORD_DATABASE,
+    "database": process.env.DATABASE,
+    "host": process.env.HOST_DATABASE,
+    "port": Number(process.env.PORT_DATABASE)
 
 })
 
