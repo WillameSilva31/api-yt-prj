8ae0e796577f4ec0fe60226307f22c956fecccfb
ChristoferMendes <christoferluizdsm@gmail.com>
2023-04-10 23:08:28 -0300

    created migration file with queries

--- src/migrations/tables.ts
+++ src/migrations/tables.ts
@@ -1,14 +1,17 @@
-import { connectWithoutExpress } from "../utils/connect"
 import { config } from 'dotenv'
-
+import { createConnection } from 'mysql2/promise'
+import { credentials } from '../mysql';
 config();
 
 const migrations = async () => {
-  const connection = await connectWithoutExpress()
+  const connection = await createConnection(credentials)
 
-  const userTable = 'CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY AUTO_INCREMENT, name varchar(255), email varchar(255) unique, password varchar(255))'
-  connection.query(userTable)
+  const userTable = 'CREATE TABLE IF NOT EXISTS users (user_id INT PRIMARY KEY AUTO_INCREMENT, name VARCHAR(255), email VARCHAR(255) UNIQUE, password VARCHAR(255))'
 
+  const videosTable = 'CREATE TABLE IF NOT EXISTS videos (video_id INT PRIMARY KEY AUTO_INCREMENT, user_id INT NOT NULL, title VARCHAR(255), description VARCHAR(255), FOREIGN KEY (user_id) REFERENCES users(user_id))'
+
+  await connection.query(userTable)
+  await connection.query(videosTable)
   console.log('Users and Videos created with success!')
   process.exit()
 
